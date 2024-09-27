73befd1f337680f6243cafee33efc6d8dbf02ef4
JMyers79 <joshua.myers0311@gmail.com>
2024-09-27 15:19:20 -0400

    Initial Commit

---
+++ meal.js
@@ -0,0 +1,57 @@
+
+const entree1 = {
+    starter : "Bacon Cheese Fries",
+    main: "Beef Burger and Coleslaw",
+    dessert: "Deep Fried Ice-Cream"
+};
+
+
+
+const entree2 = {
+    starter : "Smoked Salmon On Rye",
+    main: "Vegetarian Couscous Rolls",
+    dessert: "Fruit Plate"
+};
+
+
+
+const entree3 = {
+    starter : "Chips and Hummus",
+    main: "Grilled Chicken in Hot Sauce",
+    dessert: "Chocolate Ganache Cake"
+};
+
+const entree4 ={
+    starter: "rolls and butter",
+    main: "Steak with bacon on top",
+    dessert: "Cheesecake"
+}
+
+const entree5 ={
+    starter: "Chips and Salsa",
+    main: "Grilled Fajita Quesadilla",
+    dessert: "Flan"
+}
+
+// ... add more meal objects
+
+// Add more choices and update the array
+
+const meals = [entree1, entree2, entree3, entree4, entree5];
+
+const randomMeal = function() {
+    // Get a random number between 0 and the length of the array, meals
+    let mealNumber = Math.floor(Math.random() * meals.length) 
+    showMeal(mealNumber);
+    }
+
+const showMeal = (mealNumber)=> {
+        let str = `Start with ${meals[mealNumber].starter}, 
+        followed by ${meals[mealNumber].main}, 
+        and finish off your meal with ${meals[mealNumber].dessert}`;
+        console.log(str)
+        let area = document.querySelector('.randomMeal');
+        area.innerHTML = `${str}`;
+    }
+
+    
